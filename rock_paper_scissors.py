4d230f93deb27e8cd671e91e051f5c4c37da3be3
grumpy-penguin <garron@cartnerfamily.co.uk>
2021-01-27 21:50:26 +0000

    Formatted with Black.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,43 +1,41 @@
 import random
 
-rock = '''
+rock = """
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-'''
+"""
 
-paper = '''
+paper = """
     _______
 ---'   ____)____
           ______)
           _______)
          _______)
 ---.__________)
-'''
+"""
 
-scissors = '''
+scissors = """
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-'''
+"""
 
 rps = [rock, paper, scissors]
 
-user_choice = int(input(
-    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"
-))
-
-computer_choice = random.randint(0,2)
-
-print(
-    "You chose:\n"
+user_choice = int(
+    input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n")
 )
+
+computer_choice = random.randint(0, 2)
+
+print("You chose:\n")
 
 if user_choice == 0:
     print(rock)
@@ -46,9 +44,7 @@ elif user_choice == 1:
 elif user_choice == 2:
     print(scissors)
 
-print(
-    "The Computer chose:\n"
-)
+print("The Computer chose:\n")
 
 if computer_choice == 0:
     print(rock)
@@ -58,30 +54,16 @@ elif computer_choice == 2:
     print(scissors)
 
 if user_choice == 0 and computer_choice == 2:
-    print(
-        "You Win!!!"
-    )
+    print("You Win!!!")
 elif user_choice == 2 and computer_choice == 0:
-    print(
-        "You Lose!!!"
-    )
+    print("You Lose!!!")
 elif user_choice == 2 and computer_choice == 1:
-    print(
-        "You Win!!!"
-    )
+    print("You Win!!!")
 elif user_choice == 1 and computer_choice == 2:
-    print(
-        "You Lose!!!"
-    )
+    print("You Lose!!!")
 elif user_choice == 1 and computer_choice == 0:
-    print(
-        "You Win!!!"
-    )
+    print("You Win!!!")
 elif user_choice == 0 and computer_choice == 1:
-    print(
-        "You Lose!!!"
-    )
+    print("You Lose!!!")
 else:
-    print(
-        "Its a draw"
-    )
+    print("Its a draw")
