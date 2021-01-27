6f7021a16925210b83b6f9a7f577bb1667b4a530
grumpy-penguin <garron@cartnerfamily.co.uk>
2021-01-27 21:49:25 +0000

    Updated to include details o  the challenge and code style.
    Added a new game, rock paper scissors.

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# 100 Days of Code
+# 100 Days of Code - Python
 
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
 
 This repo contains projects from my [100 Days of Code - The Complete Python Pro Bootcamp for 2021](https://www.udemy.com/course/100-days-of-code/).
 
+I'm motivated to take part in this challenge, by recent work I've been doing with Ansible. But its more than that, I've been hacking along using Google for far too long and I need to get some structured knowledge and gain to some clear examples along the way.
+
+Once, I've written and tested these projects, I apply formatting to using the [Black](https://github.com/psf/black) formatter. The README markdown documented is formatted with [Prettier](https://github.com/prettier/prettier).
+
 [Tip Calculator](tip_calculator.py) a simple calculator demonstrating data types, number manipulation, mathmatical operators and f strings.
 
 [Treasure Island](treasure_island.py) a simple game demonstrating the use of control flow.
+
+[Rock Paper Scissors](rock_paper_scissors.py) a simple game to demonstate the use of randomness.
---
+++ rock_paper_scissors.py
@@ -0,0 +1,87 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+rps = [rock, paper, scissors]
+
+user_choice = int(input(
+    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"
+))
+
+computer_choice = random.randint(0,2)
+
+print(
+    "You chose:\n"
+)
+
+if user_choice == 0:
+    print(rock)
+elif user_choice == 1:
+    print(paper)
+elif user_choice == 2:
+    print(scissors)
+
+print(
+    "The Computer chose:\n"
+)
+
+if computer_choice == 0:
+    print(rock)
+elif computer_choice == 1:
+    print(paper)
+elif computer_choice == 2:
+    print(scissors)
+
+if user_choice == 0 and computer_choice == 2:
+    print(
+        "You Win!!!"
+    )
+elif user_choice == 2 and computer_choice == 0:
+    print(
+        "You Lose!!!"
+    )
+elif user_choice == 2 and computer_choice == 1:
+    print(
+        "You Win!!!"
+    )
+elif user_choice == 1 and computer_choice == 2:
+    print(
+        "You Lose!!!"
+    )
+elif user_choice == 1 and computer_choice == 0:
+    print(
+        "You Win!!!"
+    )
+elif user_choice == 0 and computer_choice == 1:
+    print(
+        "You Lose!!!"
+    )
+else:
+    print(
+        "Its a draw"
+    )
