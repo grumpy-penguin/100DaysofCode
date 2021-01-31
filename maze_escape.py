8a2ac2aa65baecbc06416a6d182ac30bb5f1b919
grumpy-penguin <garron@cartnerfamily.co.uk>
2021-01-31 21:22:49 +0000

    Removed some old code that I hadn't meant to include.
    Formatted with black.

--- maze_escape.py
+++ maze_escape.py
@@ -3,6 +3,7 @@ def turn_right():
     turn_left()
     turn_left()
 
+
 while not at_goal():
     if not wall_on_right():
         turn_right()
@@ -12,47 +13,3 @@ while not at_goal():
         turn_left
     else:
         turn_left()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-
-def hurdle():
-    turn_left()
-    while wall_on_right():
-        move()
-    turn_right()
-    move()
-    turn_right()
-    while front_is_clear():
-        move()
-    turn_left()
-
-while not at_goal():
-    if wall_in_front():
-        hurdle()
-    else:
-        move()
